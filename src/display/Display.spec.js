422715a1df91cadd9a39a067c1628859c1930296
Kevin Schoell <schoell411@gmail.com>
2019-12-17 10:38:19 -0600

    some display tests added

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+//component to be tested
+import Display from '../display/Display';
+
+test('Display renders', () => {
+    expect(render(<Display />)).toMatchSnapshot();
+})
+
+test('mock', () => {
+    const mock = jest.fn();
+    mock('test');
+    expect(mock).toHaveBeenCalled();
+})
+
+test('displays closed state with red-led', () => {
+    const {queryByText, getByText } = render(<Display closed={true} />);
+    getByText(/closed/i);
+    queryByText(/red-led/i)
+})
