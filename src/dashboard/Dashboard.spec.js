20c2995cec737fa67ba9078fbcbe5a2ddd3863f0
Kevin Schoell <schoell411@gmail.com>
2019-12-17 10:24:24 -0600

    dashboard test added

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,9 @@
-// Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+
+//component to be tested
+import Dashboard from '../dashboard/Dashboard';
+
+test('Display renders', () => {
+    expect(render(<Dashboard />)).toMatchSnapshot();
+})
